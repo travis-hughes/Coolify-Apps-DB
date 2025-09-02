f509918ab6fb56d738bfcba3fd9a429cf0b554b1
unknown <me@travishughes.net>
2025-09-02 03:41:58 +0100

    Added fix to onlyoffice-communityserver

---
+++ Onlyoffice-CommunityServer/config/mysql/conf.d/docker-entrypoint-initdb.d/onlyoffice-initdb.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS onlyoffice CHARACTER SET "utf8" COLLATE "utf8_general_ci";
+CREATE DATABASE IF NOT EXISTS onlyoffice_mailserver CHARACTER SET "utf8" COLLATE "utf8_general_ci";
+
+FLUSH PRIVILEGES;
