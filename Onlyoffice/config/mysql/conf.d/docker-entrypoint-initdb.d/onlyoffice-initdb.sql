3c77df31032ba88c880216147757450b7dc6fc75
unknown <me@travishughes.net>
2025-09-02 06:50:18 +0100

    Added fix to onlyoffice-communityserver

--- Onlyoffice/config/mysql/conf.d/docker-entrypoint-initdb.d/onlyoffice-initdb.sql
+++ Onlyoffice/config/mysql/conf.d/docker-entrypoint-initdb.d/onlyoffice-initdb.sql
@@ -2,8 +2,6 @@
 CREATE DATABASE IF NOT EXISTS onlyoffice CHARACTER SET "utf8" COLLATE "utf8_general_ci";
 CREATE DATABASE IF NOT EXISTS onlyoffice_mailserver CHARACTER SET "utf8" COLLATE "utf8_general_ci";
 
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'my-secret-pw';
-
 -- Allow connections on all
 TO 'root'@'%'
 
